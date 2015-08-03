149d21546e44b03d61dba3c00e17d3b0caf7dac2
romain boessel <romain.boessel@businessecision.com>
2015-08-03 15:13:43 +0200

    init base api mongo bundle

---
+++ BaseApiMongoModelBundle/Document/Blockable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace OpenOrchestra\BaseApiMongoModelBundle\Document;
+
+/**
+ * Trait Blockable
+ */
+trait Blockable
+{
+    /**
+     * @var boolean $blocked
+     *
+     * @ODM\Field(type="boolean")
+     */
+    protected $blocked = false;
+
+    /**
+     * @return boolean
+     */
+    public function isBlocked()
+    {
+        return $this->blocked;
+    }
+
+    /**
+     * @param boolean $blocked
+     */
+    public function setBlocked($blocked)
+    {
+        $this->blocked = $blocked;
+    }
+
+    /**
+     * Block a client
+     */
+    public function block()
+    {
+        $this->blocked = true;
+    }
+
+    /**
+     * Unblock a client
+     */
+    public function unblock()
+    {
+        $this->blocked = false;
+    }
+}
---
+++ BaseApiMongoModelBundle/Repository/ApiClientRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OpenOrchestra\BaseApiMongoModelBundle\Repository;
+
+use OpenOrchestra\BaseApi\Model\ApiClientInterface;
+use OpenOrchestra\BaseApi\Repository\ApiClientRepositoryInterface;
+use OpenOrchestra\Pagination\MongoTrait\PaginationTrait;
+use OpenOrchestra\Repository\AbstractAggregateRepository;
+
+
+/**
+ * Class ApiClientRepository
+ */
+class ApiClientRepository extends AbstractAggregateRepository implements ApiClientRepositoryInterface
+{
+    use PaginationTrait;
+
+    /**
+     * @param string $key
+     * @param string $secret
+     *
+     * @return ApiClientInterface
+     */
+    public function findOneByKeyAndSecret($key, $secret)
+    {
+        return $this->findOneBy(array('key' => $key, 'secret' => $secret));
+    }
+}
